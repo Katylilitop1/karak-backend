e658e78db21d550f028677046656d102ed20eb5a
MarcMars2023 <marc.de.verclos@outlook.com>
2023-05-15 15:33:36 +0200

    for frontend test purpose: - add route get /users + /newGame - add route post /users + /newGame with req.body: {id:...}

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,16 @@ var router = express.Router();
 require('../models/connection');
 const { checkBody } = require('../modules/checkBody');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET users ident of a new game */
+router.get('/newGame', function(req, res, next) {
+  console.log('route get /users + /newGame');
+  res.json({ result: true, data : { _id: "id_karak_one"} });
+});
+
+/* POST users, check the ident of game */
+router.post('/joinGame', function(req, res, next) {
+  console.log('route post /users + /newGame with req.body: ', req.body);
+  res.json({ result: true, data: { info: req.body.id + ' is known' }})
 });
 
 module.exports = router;
