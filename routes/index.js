036cb33694afa2b3ed15159809adbdce12140f51
Katy Leverdez <kleverdez@hotmail.com>
2023-05-24 10:54:00 +0200

    pusher config

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,16 @@ const Meeting = require('../models/meetings')
 const Tile = require('../models/tiles')
 const Game = require('../models/games')
 
+
+const Pusher = require('pusher');
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APPID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+  useTLS: true,
+});
+
 /* GET / + /newGame, got ident of a new game */
 router.get('/newGame', async function (req, res) {
   console.log('route get / + /newGame');
@@ -275,6 +285,39 @@ router.post('/nbrFreePlayer', async function (req, res) {
   return;
 
 })
+
+
+
+//Roads for Pusher
+// Join chat
+router.put('/users/:username', (req, res) => {
+  console.log('entry put/users/:username' )
+  pusher.trigger('chat', 'join', {
+    username: req.params.username,
+  });
+
+  res.json({ result: true });
+});
+
+// Leave chat
+router.delete("/users/:username", (req, res) => {
+  console.log('entry delete/users/:username' )
+  pusher.trigger('chat', 'leave', {
+    username: req.params.username,
+  });
+
+  res.json({ result: true });
+});
+
+// Send message
+router.post('/message', (req, res) => {
+  console.log('entry post/users/:username' )
+  const message = req.body;
+
+  pusher.trigger('chat', 'message', message);
+
+  res.json({ result: true });
+});
 
 module.exports = router;
 
